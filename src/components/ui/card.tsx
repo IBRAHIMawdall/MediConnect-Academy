5db099d697d6ead9501843e24ee1e172a8cda856
ibrahim Awadallah <ibrahiemawdallah@gmail.com>
2025-09-14 11:13:59 +0000

    not yet

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
+
 import { cn } from "@/lib/utils"
 
 const Card = React.forwardRef<
@@ -31,20 +31,17 @@ CardHeader.displayName = "CardHeader"
 
 const CardTitle = React.forwardRef<
   HTMLParagraphElement,
-  React.HTMLAttributes<HTMLHeadingElement> & { asChild?: boolean }
->(({ className, asChild = false, ...props }, ref) => {
-  const Comp = asChild ? Slot : "h3";
-  return (
-    <Comp
-      ref={ref}
-      className={cn(
-        "text-2xl font-semibold leading-none tracking-tight",
-        className
-      )}
-      {...props}
-    />
-  );
-});
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <h3
+    ref={ref}
+    className={cn(
+      "text-2xl font-semibold leading-none tracking-tight",
+      className
+    )}
+    {...props}
+  />
+))
 CardTitle.displayName = "CardTitle"
 
 const CardDescription = React.forwardRef<
