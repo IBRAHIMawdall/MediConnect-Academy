8b15e058956194ab3b766a32635f05016bcd6bc9
ibrahim Awadallah <ibrahiemawdallah@gmail.com>
2025-09-14 11:09:59 +0000

    Try fixing this error: `Console Error: Error: [GoogleGenerativeAI Error]

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -7,6 +7,6 @@ export const ai = genkit({
       apiVersion: 'v1beta',
     }),
   ],
-  model: 'googleai/gemini-2.5-flash-preview',
+  model: 'googleai/gemini-1.5-flash-latest',
   temperature: 0.5,
 });
