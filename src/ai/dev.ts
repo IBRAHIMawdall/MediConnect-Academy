ccf9fb9c7187f1e0092cf94d83b0f0c796e2ec4d
ibrahim Awadallah <ibrahiemawdallah@gmail.com>
2025-09-17 21:40:56 +0000

    prepare for production\

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -6,4 +6,3 @@ import '@/ai/flows/generate-certificate.ts';
 import '@/ai/flows/explain-topic-flow.ts';
 import '@/ai/flows/research-assistant-flow.ts';
 import '@/ai/flows/suggest-course-topic-flow.ts';
-// import '@/ai/flows/generate-promo-video-flow.ts';
