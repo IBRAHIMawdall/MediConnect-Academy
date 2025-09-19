9c606d7e7a941b56a12e4f7274b58ad23b7da70b
ibrahim Awadallah <ibrahiemawdallah@gmail.com>
2025-09-19 00:52:44 +0000

    add background pic

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+       backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
        fontFamily: {
         body: ['Inter', 'sans-serif'],
         headline: ['Inter', 'sans-serif'],
